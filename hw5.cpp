f183953e197bc30f7a5b43bd3af8155f9eca6655
mbaltazar26 <52508464+mbaltazar26@users.noreply.github.com>
2019-12-03 19:36:51 -0800

    Add files via upload

--- hw5.cpp
+++ hw5.cpp
@@ -35,11 +35,86 @@ int main()
     cout << iter.get();
     cout << "Your list is: " << endl;
     cout << "(";
-   while (iter.)
+   while (iter != listy.end())
    {
-       cout << iter.get() << endl;
-       iter++
+       cout << iter.get() << ", " << endl;
+       iter.next();
    }
+    cout << iter.get() << endl;
+    cout << ")";
+    
+    //insert
+    iter = listy.begin();
+    int index;
+    int value;
+    cout << "Please give an index to insert: ";
+    cin >> index;
+    cout << endl;
+    cout << "Please give a value to insert: " ;
+    cin >> value;
+    for (int i = 0; i < index; i++)
+    {
+        iter.next();
+    }
+    listy.insert(iter, value);
+    iter = listy.begin();
+    cout << "The augmented list is: " << endl;
+    cout << "(";
+    while (iter != listy.end())
+    {
+        cout << iter.get() << ", " << endl;
+        iter.next();
+    }
+    cout << iter.get() << endl;
+    cout << ")" << endl;
+    
+    // sort
+    iter = listy.begin();
+    Iterator iter2 = iter;
+    iter2.next();
+    listy.sort(iter, iter2);
+    cout << "The sorted list is: ";
+    cout << "(";
+    while (iter != listy.end())
+    {
+        cout << iter.get() << ", " << endl;
+        iter.next();
+    }
+    cout << iter.get() << endl;
+    cout << ")";
+    
+    //reverse
+    iter = listy.begin();
+    listy.reverse(iter);
+    iter = listy.begin();
+    cout << "The reversed list is: " << endl;
+    cout << "(";
+    while (iter != listy.end())
+    {
+        cout << iter.get() << ", " << endl;
+        iter.next();
+    }
+    cout << iter.get() << endl;
+    cout << ")";
+    
+    //new list
+    List l2;
+    l2.push_back(2);
+    l2.push_back(3);
+    l2.push_back(5);
+    l2.push_back(7);
+    l2.push_back(11);
+    listy.merge(l2);
+    iter = listy.begin();
+    cout << "Here is another list: (2, 3, 5, 7, 11)" << endl;
+    cout << "The merged list is: " << endl;
+    cout << "(";
+    while (iter != listy.end())
+    {
+        cout << iter.get() << ", " << endl;
+        iter.next();
+    }
+    cout << iter.get() << endl;
     cout << ")";
     
     return 0;
