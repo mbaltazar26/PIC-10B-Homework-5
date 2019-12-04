bd4120ba71479f2624d6023ad346fb13201485c3
mbaltazar26 <52508464+mbaltazar26@users.noreply.github.com>
2019-12-03 19:15:09 -0800

    Add files via upload

--- hw5.cpp
+++ hw5.cpp
@@ -7,9 +7,40 @@
 //
 
 #include <iostream>
+#include <sstream>
+#include <string>
+#include "List.h"
+#include "Node.h"
+#include "Iterator.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    List listy;
+    int num;
+    cout << "Please input a set of nonnegative numbers for a list." << endl;
+    cout << "(Enter -1 when you are finished): " << endl;
+    cin >> num;
+    if (!cin)
+    {
+        cout << "That is not a number >:(" << endl;
+        return 0;
+    }
+    while (num > 0)
+    {
+        listy.push_back(num);
+        cout << "Next input: ";
+        cin >> num;
+    }
+    Iterator iter = listy.begin();
+    cout << iter.get();
+    cout << "Your list is: " << endl;
+    cout << "(";
+   while (iter.)
+   {
+       cout << iter.get() << endl;
+       iter++
+   }
+    cout << ")";
+    
     return 0;
 }
